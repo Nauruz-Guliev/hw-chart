78165d0eae6370354b1418c1448bcd96f7eb8692
Nauruz-Guliev <nauruz9248327@gmail.com>
2023-06-02 09:32:24 +0300

    + image

--- app/src/main/java/ru/example/gnt/hw_custom_chart/MainActivity.kt
+++ app/src/main/java/ru/example/gnt/hw_custom_chart/MainActivity.kt
@@ -1,11 +1,25 @@
 package ru.example.gnt.hw_custom_chart
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.example.gnt.hw_custom_chart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.chartView.values =
+            listOf(
+                Pair(Color.RED, 40f),
+                Pair(Color.GREEN, 20f),
+                Pair(Color.BLUE, 30f),
+                Pair(Color.YELLOW, 10f),
+            )
+
     }
 }
